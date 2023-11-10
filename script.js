c0be3425529223090afef40b3c338471c8540aad
Aranarora <aranarora007@gmail.com>
2023-11-10 15:08:14 -0800

    Add const tiles with leaflet tile layer to show on map
    Add the tiles variable to the map div

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const map = L.map('map').setView([51.505, -0.09], 13);
         const attribution= '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
         const tileUrl = 'https://api.mapbox.com/styles/v1/aroraaran/clot5qbee007c01r60duwcwae/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXJvcmFhcmFuIiwiYSI6ImNsb3QzdXV3MDA2MjAybG82Y3NlaGZxZTUifQ.L0hJIwM_Lhr2zaQwEcAOpw';
-
+        const tiles = L.tileLayer(tileUrl, { attribution });
+        tiles.addTo(map);
